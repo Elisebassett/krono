aebd8a2475b8100b7b3417926b5248e1a451d954
Elisebassett <elise@iileaders.com>
2017-12-18 15:30:18 -0700

    small fixes before moving on to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218212336) do
+ActiveRecord::Schema.define(version: 20171218221313) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20171218212336) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.time "start_time"
+    t.time "end_time"
   end
 
 end
