398ecea66f62f5fac6d2d85eaeb2b0a46414ea94
Elisebassett <elise@iileaders.com>
2017-12-19 10:16:20 -0700

    fixes #10 fixes #13

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  def name
+  	self.first_name + ' ' + self.last_name
+  end
+
+
+end
