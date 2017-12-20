06f85554d00cde6bcb262f76443d3386c9f1126a
Elisebassett <elise@iileaders.com>
2017-12-20 11:13:54 -0700

    fixes #28

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,12 +40,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :avatar, :sleep, :rise]) 
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :avatar, :sleep, :rise])
   end
 
   # The path used after sign up.
