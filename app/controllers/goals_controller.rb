a3611a982167f0fc784f4d2a0e33e4dbb5bbe983
Elisebassett <elise@iileaders.com>
2017-12-21 14:52:32 -0700

    fixes #42 fixes #31

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -39,83 +39,55 @@ class GoalsController < ApplicationController
 
   def set_goal_event
     @goal = Goal.find(params[:id])
-    tomorrow_events = current_user.events.where('DATE(start) = ?', DateTime.tomorrow).sort_by &:start
-
     tomorrow = DateTime.tomorrow
+    tomorrow_events = current_user.events.where('DATE(start) = ?', tomorrow).sort_by &:start
 
     rise = current_user[:rise].change(year: tomorrow.year, month: tomorrow.month, day: tomorrow.day)
-    p 'RISE'
-    p current_user[:rise]
-    p rise
-
-    p 'TOMORROW EVENTS'
-    p tomorrow_events
+    sleep = current_user[:sleep].change(year: tomorrow.year, month: tomorrow.month, day: tomorrow.day)
 
     tomorrow_events.length.times do |i|
       
-      if i == 0
-        p 'FIRST APPOINTMENT'
-        p i
-        
+      if i == 0 #if it's the first event of the day
+#time_allocation is saved in db as minutes. The comparison eqution results in seconds, thus the division by 60
+        if @goal[:time_allocation] + 20 < (tomorrow_events[i][:start].to_time - rise)/60
 
-        difference = tomorrow_events[i][:start].to_time - rise
-
-        p 'MORNING DIFFERENCE'
-        p tomorrow_events[i][:start]
-        p rise
-        p difference
-
-        p 'TIME ALLOCATION'
-        p (@goal[:time_allocation]) + 20
-
-        p 'SET GOAL?'
-        p (@goal[:time_allocation]) + 20 < (tomorrow_events[i][:start].to_time - rise)/60
-
-
-        if (@goal[:time_allocation]) + 20 < (tomorrow_events[i][:start].to_time - rise)/60
-
-         
-          
           p 'SET A MORNING GOAL'
 
         end #set goal before first appointment
-      end
-
-      if i > 0
-        #time_allocation is saved in db as minutes. The comparison eqution results in seconds, thus the division by 60
-        p 'IN BETWEEN EVENTS'
-        p ' '
-
-        if (@goal[:time_allocation]) + 20 < (tomorrow_events[i][:start].to_time - tomorrow_events[i-1][:end].to_time)/60
-
-          # difference = tomorrow_events[i][:start] - tomorrow_events[i-1][:end]
-          # p 'START TIME'
-          # p tomorrow_events[i][:start]
-          # p 'END TIME'
-          # p tomorrow_events[i-1][:end]
-          # p 'THIS IS THE DIFFERENCE'
-          # p difference
-          
-
-
-          p 'SET A GOAL!!!'
-        end #set the goal between appointments
-
-
+      
+      elsif i == (tomorrow_events.length - 1)
+        p 'LAST EVENT'
         
-      end #if it's not the first event
+        if @goal[:time_allocation] + 20 < (tomorrow_events[i][:start].to_time - tomorrow_events[i-1][:end].to_time)/60
+          
+          p 'SET A GOAL!!!'
+        end #set the goal between appointments    
+
+        if @goal[:time_allocation] + 20 < (sleep - tomorrow_events[i][:end].to_time)/60
+          
+          p 'SET EVENING GOAL'
+
+        end#set evening goal
+
+      elsif i > 0 #if it's not the first or last event
+
+        if @goal[:time_allocation] + 20 < (tomorrow_events[i][:start].to_time - tomorrow_events[i-1][:end].to_time)/60
+          
+          p 'SET A GOAL!!!'
+        end #set the goal between appointments    
+      end #end of elseif
     end #times loop
   end# end of set_goal_event
-
-  def calculate_event_params
-    
-  end
 
 
   private
 
     def set_goal
       @goal = Goal.find(params[:id])
+    end
+
+    def set_event_params
+      
     end
 
     def goal_params
